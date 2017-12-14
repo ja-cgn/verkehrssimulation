dd084475fa2d85f4532811ac6b29a3685cb94bd7
Jean Ozmaden <cozmaden@gmail.com>
2017-12-14 13:58:31 +0100

    Grammar fix, add items to project file

--- Aufgabenblock_3/Streckenende.cpp
+++ Aufgabenblock_3/Streckenende.cpp
@@ -21,7 +21,7 @@ Streckenende::~Streckenende()
 
 void Streckenende::vBearbeiten()
 {
-	cout << "\nSTRECKENDE EXCEPTION! vBearbeiten() wurde aufgerufen!\n";
+	cout << "\nSTRECKENENDE EXCEPTION! vBearbeiten() wurde aufgerufen!\n";
 	cout << "Fahzeug: " << this->p_pFhzg->sGetName() << "\nWeg: " << this->p_pWeg->sGetName() << endl;
 
 	//Rufe die Abgabe Fkt, wenn diese Ausnahme aufgetreten ist, um gezielt dieses Fahrzeug zu loeschen
